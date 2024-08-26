48a63340f1ebae53f6f0e8fdd5817eacf07484eb
paintennn <thinhphan1171@gmail.com>
2024-08-26 16:25:53 +0700

    props

---
+++ src/components/DisplayInfor.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class DisplayInfor extends React.Component {
+    render() {
+
+        //destructuring array/object
+        const { age, name } = this.props;//object
+        //prop
+        return (
+            <div>
+                <div>My name is: {name}</div>
+                <div>My age is: {age}</div>
+            </div>
+        )
+    }
+}
+
+export default DisplayInfor
--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -3,6 +3,7 @@
 import React from "react";
 import { applyMiddleware } from "redux";
 import UserInfor from "./UserInfor";
+import DisplayInfor from "./DisplayInfor";
 
 
 //class component
@@ -11,9 +12,15 @@ class MyComponent extends React.Component {
 
     //JSX
     render() {
+        const myInfor = ['a', 'b', 'c']
         return (
             <div>
                 <UserInfor />
+                <br />
+                <br />
+                <DisplayInfor name='Phan Huu Thinh' age='50' />
+                <hr />
+                <DisplayInfor name='pain' age={20} myInfor={myInfor} />
             </div>
         );
     }
