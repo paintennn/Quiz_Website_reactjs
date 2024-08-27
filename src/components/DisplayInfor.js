f20897019819597284eed03b4159a925e0d9a074
paintennn <thinhphan1171@gmail.com>
2024-08-27 15:27:09 +0700

    variables with JSX

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './DisplayInfor.scss'
-import logo from './../logo.svg'
+import logo1 from './../logo.svg'
 class DisplayInfor extends React.Component {
 
     state = {
@@ -20,9 +20,11 @@ class DisplayInfor extends React.Component {
         const { listUsers } = this.props;//object
         //prop
         // console.table(listUsers)
+
+        //component js: template + logic js
         return (
             <div className="display-infor-container">
-                <img src={logo} />
+                <img src={logo1} />
                 <div>
                     <span onClick={() => { this.handleShowHide() }}>
                         {this.state.isShowListUser === true ? 'Hide List User:' : 'Show List User:'}
