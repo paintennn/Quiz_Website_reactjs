fcc4d5919d2989d151df4dc74132800012e54e63
paintennn <thinhphan1171@gmail.com>
2024-09-03 21:46:01 +0700

    update index

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import './index.scss';
 import './Layout.js';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Layout from './Layout.js';
 import { Provider } from 'react-redux';
-import {store, persistor} from './redux/store.js';
+import { store, persistor } from './redux/store.js';
 import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -19,12 +19,12 @@ root.render(
       </BrowserRouter>
     </PersistGate>
   </Provider>
-  
+
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals...
 reportWebVitals();
 
 
